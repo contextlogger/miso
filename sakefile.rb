bc75aef91a985cc7586be517a851e78cac698f79
tero.hasu <tero.hasu@hut.fi>
2009-01-03 00:44:44 +0200

    version 1.95 maybe
    darcs-hash:20090102224444-f0f46-00d779cdba12d4449f6c497622e9d9eedb5844be.gz

--- sakefile.rb
+++ sakefile.rb
@@ -1,6 +1,35 @@
 # -*- ruby -*-
 # sake variant sake3
 
+#
+# sakefile.rb
+#
+# Copyright 2008 Helsinki Institute for Information Technology (HIIT)
+# and the authors. All rights reserved.
+#
+# Authors: Tero Hasu <tero.hasu@hut.fi>
+#
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # The build tool driving this makefile is a custom one. The required
 # version has not yet been released.
 
@@ -17,7 +46,7 @@ $basename = "miso"
 
 $proj = Sake::Project.new(:basename => $basename,
                           :name => "Miso Library for PyS60",
-                          :version => [1, 94],
+                          :version => [1, 95],
                           # This is a test UID.
                           :uid => Sake::Uid.v8($uid_v8),
                           :vendor => "HIIT")
