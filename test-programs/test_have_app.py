248d62aa2a6ad43e2e34606eb30844e24fd2d8b7
tero.hasu <tero.hasu@hut.fi>
2009-11-23 13:26:06 +0200

    test program for is app installed
    darcs-hash:20091123112606-f0f46-e167d062f6bfc035e688f23cc760149696a97c06.gz

---
+++ test-programs/test_have_app.py
@@ -0,0 +1,7 @@
+from miso import is_app_installed
+
+print(is_app_installed(0xe3462346)) # Miso (3rd ed, PyS60 1)
+print(is_app_installed(0xe3462347)) # Miso (3rd ed, PyS60 2)
+print(is_app_installed(0xe8460002)) # logger
+print(is_app_installed(0x08460006)) # logger watchdog
+print(is_app_installed(0xe8460009)) # logger C++ lib
